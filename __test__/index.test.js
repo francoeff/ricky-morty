fd0846b6cc799a4209ea5334887b650d9d2e1ee5
Franco Figueroa Flores <franco.eff95@gmail.com>
2023-10-03 10:50:34 -0300

    test: add test for runChallenge

---
+++ __test__/index.test.js
@@ -0,0 +1,46 @@
+import { runChallenge } from '../src';
+
+describe('Result of Challenge', () => {
+  test('should run two exercises', async () => {
+    const result = await runChallenge(false);
+    const firstExercise = result[0];
+    const secondExercise = result[1];
+
+    expect(firstExercise.results).toEqual([
+      { char: 'l', count: 82, resource: 'location' },
+      { char: 'e', count: 88, resource: 'episode' },
+      { char: 'c', count: 494, resource: 'character' },
+    ]);
+
+    expect(firstExercise.in_time).toBe(true);
+
+    expect(secondExercise.results[0]).toEqual({
+      name: 'Pilot',
+      episode: 'S01E01',
+      locations: [
+        'Citadel of Ricks',
+        'Bepis 9',
+        'Earth (C-137)',
+        'unknown',
+        'Interdimensional Customs',
+        'Earth (Replacement Dimension)',
+        "Worldender's lair",
+      ],
+    });
+
+    expect(secondExercise.results[1]).toEqual({
+      name: 'Lawnmower Dog',
+      episode: 'S01E02',
+      locations: [
+        'Citadel of Ricks',
+        'Earth (C-137)',
+        'unknown',
+        "Mr. Goldenfold's dream",
+        "Snuffles' Dream",
+        'Earth (Replacement Dimension)',
+      ],
+    });
+
+    expect(secondExercise.in_time).toBe(true);
+  });
+});
