5812741e2857a3d611af879d32a3e712f9b6db02
Franco Figueroa Flores <franco.eff95@gmail.com>
2023-10-03 02:09:38 -0300

    test: add test for counterCharFromNames

--- __test__/helper.test.js
+++ __test__/helper.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getAllPagesOfApi } from '../src/services/helpers.js';
+import { counterCharFromNames, getAllPagesOfApi } from '../src/services/helpers.js';
 
 jest.mock('axios');
 
@@ -61,6 +61,38 @@ describe('getAllPagesOfApi', () => {
 
     const result = await getAllPagesOfApi('https://rickandmortyapi.com/api/character');
     expect(result).toEqual([]);
+    expect(axios.get).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character?page=1');
+  });
+});
+
+describe('counterCharFromNames', () => {
+  test('should return a counter of a char in a list of names', async () => {
+    const mockResponse = {
+      data: {
+        info: { pages: 1 },
+        results: [{ name: 'Rick' }, { name: 'Morty' }, { name: 'Summer' }, { name: 'Beth' }, { name: 'Jerry' }],
+      },
+    };
+
+    axios.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await counterCharFromNames('character', 'r');
+    expect(result).toEqual({ char: 'r', count: 5, resource: 'character' });
+    expect(axios.get).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character?page=1');
+  });
+
+  test('should return a counter of a char in a list of names with no results', async () => {
+    const mockResponse = {
+      data: {
+        info: { pages: 1 },
+        results: [],
+      },
+    };
+
+    axios.get.mockResolvedValueOnce(mockResponse);
+
+    const result = await counterCharFromNames('character', 'c');
+    expect(result).toEqual({ char: 'c', count: 0, resource: 'character' });
     expect(axios.get).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character?page=1');
   });
 });
